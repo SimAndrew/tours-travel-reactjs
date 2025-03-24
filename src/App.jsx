df16f3cc6341159e8653cd68fe7136c7f72dfd3d
Andrew Siminiuс <44125451+SimAndrew@users.noreply.github.com>
2025-03-24 18:28:47 +0200

    feat: display list of tours from external api, add loading spinner

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,41 @@
+import { useEffect, useState } from 'react';
+import Loading from './Loading.jsx';
+import Tours from './Tours.jsx';
+
 const url = 'https://www.course-api.com/react-tours-project';
 
-const App = () => {
-	return <h2>Tours Starter</h2>;
-};
+function App() {
+	const [isLoading, setIsLoading] = useState(true);
+	const [tours, setTours] = useState([]);
+
+	const fetchTours = async () => {
+		setIsLoading(true);
+		try {
+			const response = await fetch(url);
+			const tours = await response.json();
+			setTours(tours);
+		} catch (error) {
+			console.log(error);
+		}
+		setIsLoading(false);
+	};
+
+	useEffect(() => {
+		fetchTours();
+	}, []);
+
+	if (isLoading) {
+		return (
+			<main>
+				<Loading />
+			</main>
+		);
+	}
+
+	return (
+		<main>
+			<Tours tours={tours} />
+		</main>
+	);
+}
 export default App;
--- src/Tour.jsx
+++ src/Tour.jsx
@@ -1,5 +1,14 @@
-const Tour = () => {
-	return <h2>Tour</h2>;
+const Tour = ({ id, image, info, name, price }) => {
+	return (
+		<article className="single-tour">
+			<img className="img" src={image} alt={name} />
+			<span className="tour-price">{price}</span>
+			<div className="tour-info">
+				<h5>{name}</h5>
+				<p>{info}</p>
+			</div>
+		</article>
+	);
 };
 
 export default Tour;
