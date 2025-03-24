1af20e9a374e9b6b7d84c288fe816255f54b4ce3
Andrew Siminiuс <44125451+SimAndrew@users.noreply.github.com>
2025-03-24 18:45:00 +0200

    feat: remove a tour from the list of tours by pressing the button

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,11 @@ function App() {
 	const [isLoading, setIsLoading] = useState(true);
 	const [tours, setTours] = useState([]);
 
+	const removeTour = (id) => {
+		const newTours = tours.filter((tour) => tour.id !== id);
+		setTours(newTours);
+	};
+
 	const fetchTours = async () => {
 		setIsLoading(true);
 		try {
@@ -34,7 +39,7 @@ function App() {
 
 	return (
 		<main>
-			<Tours tours={tours} />
+			<Tours tours={tours} removeTour={removeTour} />
 		</main>
 	);
 }
