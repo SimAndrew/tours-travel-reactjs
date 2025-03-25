5910d1cbc1cd6c6e4c1f8a1369d226e981819ec5
Andrew Siminiuс <44125451+SimAndrew@users.noreply.github.com>
2025-03-25 15:21:50 +0200

    feat: clicking on read more will display a detailed tour description.

--- src/Tour.jsx
+++ src/Tour.jsx
@@ -1,11 +1,25 @@
+import { useState } from 'react';
+
 const Tour = ({ id, image, info, name, price, removeTour }) => {
+	const [readMore, setRreadMore] = useState(false);
+
 	return (
 		<article className="single-tour">
 			<img className="img" src={image} alt={name} />
 			<span className="tour-price">{price}</span>
 			<div className="tour-info">
 				<h5>{name}</h5>
-				<p>{info}</p>
+				<p>
+					{readMore ? info : `${info.substring(0, 200)}...`}
+
+					<button
+						className="info-btn"
+						type="button"
+						onClick={() => setRreadMore(!readMore)}
+					>
+						{readMore ? 'Show less' : 'Read more'}
+					</button>
+				</p>
 				<button
 					className="btn btn-block delete-btn"
 					type="button"
